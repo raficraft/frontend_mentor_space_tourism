93b1a19225a7f02a5d0289ab4daabeaca019fdf0
raficraft <raficraft@gmail.com>
2022-05-30 09:55:51 +0200

    add Helmet package for manage title page and other

--- src/App.js
+++ src/App.js
@@ -5,27 +5,34 @@ import Home from "./engine/pages/Home/Home";
 import Destination from "./engine/pages/Destination/Destination";
 import Crew from "./engine/pages/Crew/Crew";
 import Technology from "./engine/pages/Technology/Technology";
+import Helmet from "react-helmet";
 
 function App() {
   const location = useLocation();
 
   return (
-    <div
-      className={`App bg_main ${
-        location.pathname === "/"
-          ? "bg_home"
-          : `bg_${location.pathname.substring(1)}`
-      }`}
-    >
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/destinations" element={<Destination />} />
-          <Route path="/crew" element={<Crew />} />
-          <Route path="/technology" element={<Technology />} />
-        </Routes>
-      </Layout>
-    </div>
+    <>
+      <Helmet>
+        <title>Front-end mentor challenge - Space tourism</title>
+        <link rel="icon" href="/assets/favicon.ico" />
+      </Helmet>
+      <div
+        className={`App bg_main ${
+          location.pathname === "/"
+            ? "bg_home"
+            : `bg_${location.pathname.substring(1)}`
+        }`}
+      >
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/destinations" element={<Destination />} />
+            <Route path="/crew" element={<Crew />} />
+            <Route path="/technology" element={<Technology />} />
+          </Routes>
+        </Layout>
+      </div>
+    </>
   );
 }
 
